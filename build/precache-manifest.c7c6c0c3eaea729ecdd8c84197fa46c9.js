1a896c8b55e03fe70311e275fe7168e9e2ad40af
AdeFuaad <adesholafuaad@gmail.com>
2023-10-12 22:32:19 -0400

    client changes

---
+++ build/precache-manifest.c7c6c0c3eaea729ecdd8c84197fa46c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ebf9680b90d05e9ca028d9652b39d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360057cb38ad6e33eea3",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "5df2e19ff142d58f2e0f",
+    "url": "/static/css/main.1cdc226c.chunk.css"
+  },
+  {
+    "revision": "360057cb38ad6e33eea3",
+    "url": "/static/js/2.6b6ff2da.chunk.js"
+  },
+  {
+    "revision": "b002637b690670ff2901",
+    "url": "/static/js/3.dfe83729.chunk.js"
+  },
+  {
+    "revision": "5df2e19ff142d58f2e0f",
+    "url": "/static/js/main.e75ffe03.chunk.js"
+  },
+  {
+    "revision": "2542b6befa585976a9a7",
+    "url": "/static/js/runtime~main.1082e1f2.js"
+  }
+]);
